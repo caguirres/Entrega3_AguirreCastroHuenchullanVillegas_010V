b6f9018bee6d795499df1d92031446600a643e11
caguirres <c.aguirres@alumnos.duoc.cl>
2020-11-15 20:08:03 -0300

    modelos v2

--- catalogo/admin.py
+++ catalogo/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
-
 # Register your models here.
+from . models import Usuario, Estado, Comensal, Locatario, Administrador, Local, Menu, Calificacion, Resenhas, Planes
+
+admin.site.register(Usuario)
+admin.site.register(Estado)
+admin.site.register(Comensal)
+admin.site.register(Locatario)
+admin.site.register(Administrador)
+admin.site.register(Local)
+admin.site.register(Menu)
+admin.site.register(Calificaciones)
+admin.site.register(resenhas)
+admin.site.register(Planes)
