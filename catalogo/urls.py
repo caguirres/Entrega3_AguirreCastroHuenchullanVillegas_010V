4b358713380269f4df92138bcefb7dec7e406a76
caguirres <c.aguirres@alumnos.duoc.cl>
2020-11-16 02:19:14 -0300

    creando pagina de inicio

--- catalogo/urls.py
+++ catalogo/urls.py
@@ -2,5 +2,11 @@ from django.urls import path
 from . import views
 
 urlpatterns=[
-
+	path('',views.inicio,name='inicio'),
+	path('/templates/galeria',views.galeria,name='galeria'),
+	path('/templates/qs',views.qs,name='qs'),
 ]
+
+	
+
+
--- catalogo/views.py
+++ catalogo/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
+from .models import Usuario, Estado, Comensal, Locatario, Administrador, Local, Menu, Calificacion, Resenhas, Planes
 
 # Create your views here.
+def inicio(request):
+
+	return render(
+		request,
+		'inicio.html',)
+
+def galeria(request):
+
+	return render(
+		request,
+		'galeria.html',)
+
+def qs(request):
+
+	return render(
+		request,
+		'qs.html',)
+
+
